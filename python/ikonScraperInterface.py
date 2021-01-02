f8f4c664dcbcb7982ced2386973fd34b55ed977e
Jake Johnson <jjohnson11096@gmail.com>
2021-01-01 22:53:22 -0700

    checks for brighton on 25

--- python/ikonScraperInterface.py
+++ python/ikonScraperInterface.py
@@ -307,8 +307,8 @@ def checkBrighton(driver):
 
 	selectMonth(driver, monthsToCheck[2], 2021)
 
-	if isDayAvailable(driver, monthsToCheck[2], 27, year):
+	if isDayAvailable(driver, monthsToCheck[2], 25, year):
 		# reserve day
-		reserveDay(driver, monthsToCheck[2], 27, year)
+		reserveDay(driver, monthsToCheck[2], 25, year)
 		# send alert
 		emailInterface.sendEmailAlert("jjohnson11096@gmail.com", "Brighton", monthsToCheck[2], "27", "2021", "Saturday")
